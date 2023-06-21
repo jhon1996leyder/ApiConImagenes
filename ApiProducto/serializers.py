1a5a9417c4906cf18b144812d68b857686de62ea
jhon_leyder_cruz <99480658+jhon1996leyder@users.noreply.github.com>
2023-06-20 19:27:39 -0500

    Delete ApiProducto directory

--- ApiProducto/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Producto
-
-class ProductosSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Producto
-        fields = ('id', 'nombre_produc', 'precio_produc', 'descripcion_produc', 'imagen_produc')
--- ApiProducto/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from .api import ProductoviewSet
-
-router = routers.DefaultRouter()
-router.register('api/producto', ProductoviewSet, 'producto')
-
-urlpatterns = router.urls
